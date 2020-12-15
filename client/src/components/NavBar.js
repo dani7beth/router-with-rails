3cc87af3fcabb5c5eec4ccabc095a3868ea20dca
dani7beth <danielledona28@gmail.com>
2020-12-15 11:15:36 -0700

    models setup

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,11 +1,22 @@
 import { Link } from "react-router-dom"
+import {Nav, Navbar} from 'react-bootstrap';
 
 export default () =>{
     return(
-         <div>
-             <Link to="/">Home</Link>
-             <Link to="/things">Things</Link>
+         <Navbar expand="lg">
+             <Navbar.Brand>React Router App</Navbar.Brand>
+             <Navbar.Toggle aria-controls="basic-navbar-nav" />
+             <Navbar.Collapse id='basic-navbar-nav'>
+                 <Nav className='mr-auto'>
+                     <Nav.Link><Link to="/">Home</Link></Nav.Link>
+                     {/* <Nav.Link><Link to="/things">Things</Link></Nav.Link> */}
+                 </Nav>
+             
+             </Navbar.Collapse>
+             
+         </Navbar>
+             
 
-         </div>
+         
     ) 
  }
